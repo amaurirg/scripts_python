8588efed69023ad4fdc2ca6da78c4935ab808bff
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2017-09-28 01:03:17 -0300

    adicionando opção de descrição e browser

--- git_bot.py
+++ git_bot.py
@@ -1,6 +1,7 @@
 from decouple import config
 from github import Github
 import os
+import webbrowser
 
 
 github_username = config('username')
@@ -13,13 +14,15 @@ print("Cria repositório no Github.\n")
 
 while True:
     nome_repo = input("Nome do repositório e da pasta a serem criados: ")
+    descricao = input("Descrição do repositório no Github (opcional): ")
     if nome_repo in [repo.name for repo in github_api.get_repos()] or os.path.exists(nome_repo):
-        print("Esse repositório (e|ou) a pasta já existe(m). Escolha outro nome.\n")
+        print("\nEsse repositório (e|ou) a pasta já existe(m). Escolha outro nome.\n")
     else:
         criando_repo = github_api.create_repo(nome_repo)
+        github_api.get_repo(nome_repo).edit(description=descricao)
         os.mkdir(nome_repo)
         if nome_repo in [repo.name for repo in github_api.get_repos()] and os.path.exists(nome_repo):
-            print("Repositório e pasta criados com sucesso!\n")
+            print("\nRepositório e pasta criados com sucesso!\n")
         break
 
 local = os.path.abspath(nome_repo)
@@ -32,3 +35,11 @@ os.system('git add .')
 os.system('git commit -m "first commit"')
 os.system('git remote add origin git@github.com:{}/{}.git'.format(github_username, nome_repo))
 os.system('git push -u origin master')
+
+url_repo = github_api.get_repo(nome_repo).html_url
+print("\nURL do repositório:", url_repo)
+resposta = input("Deseja abrir o repositório no navegador (s|n)?")
+
+if resposta == 's':
+    b = webbrowser.get('google-chrome')
+    b.open(url_repo)
