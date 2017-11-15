5c77d38139bdd6f9b1909de5f4e366f00c58b464
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2017-11-15 17:34:55 -0200

    Adicionando argparse para não criar pasta.

--- git_bot.py
+++ git_bot.py
@@ -2,7 +2,14 @@ from decouple import config
 from github import Github
 import os
 import webbrowser
+import argparse
 
+
+parser = argparse.ArgumentParser(description='Opções de repositórios no Github.')
+parser.add_argument('-p', '--pasta', action='store_true',
+                    help='Ignora a criação da pasta e cria somente o repositório no Github.')
+
+args = parser.parse_args()
 
 github_username = config('username')
 github_password = config('password')
@@ -10,22 +17,42 @@ github_password = config('password')
 github_api = Github(github_username, github_password).get_user()
 
 os.system('clear')
-print("Esse script cria uma pasta e um repositório no Github.\n")
+print("Esse script cria uma pasta(opcional) e um repositório no Github.\n")
 
-while True:
-    nome_repo = input("Nome do repositório e da pasta a serem criados: ")
+nome_repo = os.path.basename(os.getcwd())
+print(nome_repo)
+
+local = os.path.abspath(os.getcwd())
+print(local)
+
+if args.pasta:
     descricao = input("Descrição do repositório no Github (opcional): ")
-    if nome_repo in [repo.name for repo in github_api.get_repos()] or os.path.exists(nome_repo):
+    if nome_repo in [repo.name for repo in github_api.get_repos()]:
         print("\nEsse repositório (e|ou) a pasta já existe(m). Escolha outro nome.\n")
     else:
         criando_repo = github_api.create_repo(nome_repo)
         github_api.get_repo(nome_repo).edit(description=descricao)
-        os.mkdir(nome_repo)
-        if nome_repo in [repo.name for repo in github_api.get_repos()] and os.path.exists(nome_repo):
-            print("\nRepositório e pasta criados com sucesso!\n")
-        break
+        if nome_repo in [repo.name for repo in github_api.get_repos()]:
+            print("\nRepositório criado com sucesso!\n")
+        else:
+            print("\nO repositório não foi criado.")
 
-local = os.path.abspath(nome_repo)
+else:
+    while True:
+        nome_repo = input("Nome do repositório e da pasta(opcional) a serem criados: ")
+        descricao = input("Descrição do repositório no Github (opcional): ")
+        if nome_repo in [repo.name for repo in github_api.get_repos()] or os.path.exists(nome_repo):
+            print("\nEsse repositório (e|ou) a pasta já existe(m). Escolha outro nome.\n")
+        else:
+            criando_repo = github_api.create_repo(nome_repo)
+            github_api.get_repo(nome_repo).edit(description=descricao)
+            os.mkdir(nome_repo)
+            if nome_repo in [repo.name for repo in github_api.get_repos()] and os.path.exists(nome_repo):
+                print("\nRepositório e pasta criados com sucesso!\n")
+            break
+
+# local = os.path.abspath(nome_repo)
+# print(local)
 os.chdir(local)
 with open('.gitignore', 'w') as arquivo:
     arquivo.write('.env\n.venv\n.gitignore\n.idea\ndb.sqlite3\n*pyc\n__pycache__\n')
